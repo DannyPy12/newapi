b7ab6a99b91f3d19f0ded0fdf2be67dc5661f7c7
DannyPy12 <158075707+DannyPy12@users.noreply.github.com>
2024-07-23 09:52:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Configura CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // URL de tu frontend
+  origin: 'https://onlywater.neuroseeq.com', // URL de tu frontend
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Si usas cookies necesitas esta línea
 }));
